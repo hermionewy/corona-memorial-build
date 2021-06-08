9553936a40005d310349ed237e3d16b3ba33ab50
hermionewy <wuyan.sysu@gmail.com>
2021-06-08 10:48:37 -0400

    first

---
+++ precache-manifest.839ec97dfae6bd583c9847371de08de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf789d476757135d9b069576b2a256c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9ee11dcbf3d66a1ff70",
+    "url": "./static/css/main.f1296e3e.chunk.css"
+  },
+  {
+    "revision": "f2e377c5c578d53af2a6",
+    "url": "./static/js/2.e3499a5d.chunk.js"
+  },
+  {
+    "revision": "e6bebf7a913fa800df6d31d0e7a574fd",
+    "url": "./static/js/2.e3499a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ee11dcbf3d66a1ff70",
+    "url": "./static/js/main.e3f6bb14.chunk.js"
+  },
+  {
+    "revision": "df618e00e6074de83808",
+    "url": "./static/js/runtime-main.42e47562.js"
+  }
+]);
